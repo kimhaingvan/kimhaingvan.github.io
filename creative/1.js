a89cbda4579814ca486bf4e2703fc94db9786dd5
Nguyen Van Kim Hai <38590797+kimhaingvan@users.noreply.github.com>
2018-09-09 23:57:57 +0700

    cz
    czx

---
+++ creative/1.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+	$(window).scroll(function(){
+		if($('html').scrollTop() > 100)
+		{
+		   	$('.navmenu').addClass('nentrang');
+		   	$('.menutrai').addClass('chucam');
+		   	$('.menuphai').addClass('den');
+		}
+		else
+		{
+				$('.navmenu').removeClass('nentrang');
+		   	$('.menutrai').removeClass('chucam');
+		   	$('.menuphai').removeClass('den');
+		}
+     });
+});
