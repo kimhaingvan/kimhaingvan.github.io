d7d76a6f3f4f5ae3e6d4d86bee1b4cc39b010723
Nguyen Van Kim Hai <38590797+kimhaingvan@users.noreply.github.com>
2019-07-27 17:03:47 +0700

    Add files via upload

---
+++ userreact/precache-manifest.5408c81a956ffb4ed9d3688f911c2acd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e0d651056c7b5481032dec65ceab15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65e1a00a060b5203f257",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "17bc4a37e6eca840ab75",
+    "url": "/static/js/2.f37fb862.chunk.js"
+  },
+  {
+    "revision": "65e1a00a060b5203f257",
+    "url": "/static/js/main.93e84a05.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
